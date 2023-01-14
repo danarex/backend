59149cffb5880b4e00345949ed64671b053dc780
danarex <109385038+danarex@users.noreply.github.com>
2023-01-14 20:45:05 -0300

    Update RedesSocialesController.java

--- src/main/java/com/portfolio/dana/Controller/RedesSocialesController.java
+++ src/main/java/com/portfolio/dana/Controller/RedesSocialesController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins= "http://localhost:4200")
+@CrossOrigin(origins ={"https://frontenddana.web.app/", "http://localhost:4200"})
 public class RedesSocialesController {
     @Autowired IRedesSocialesService  iredessocialesService;
     
