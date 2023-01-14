909c4661e0d0dc5a7aaf0e88b01c084ab45fa165
danarex <109385038+danarex@users.noreply.github.com>
2023-01-14 20:43:32 -0300

    Update CExperiencia.java

--- src/main/java/com/portfolio/dana/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/dana/Controller/CExperiencia.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins ={"https://frontenddana.web.app/", "http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     ImpExperienciaService impExperienciaService;
